89808c7888d8b932bb2a0c0c3d99c80cd63322b1
sandrockjustin <sandrock.justin@gmail.com>
2025-03-26 14:10:48 -0500

    create / examples of pattern matching

---
+++ main/lib/Pattern_Matching/match.ex
@@ -0,0 +1,31 @@
+defmodule Match do
+
+  def destructureExample do
+    {a, b, c} = {:hello, "world", 42}
+    IO.inspect([a, b, c]) # Notice the use of inspect here
+  end
+
+  def dissectList(x) do
+    [head | tail] = x
+    IO.puts("Head: #{head} | Tail: #{tail}")
+  end
+
+  def responseOk do
+    {:ok, contents} = {:ok, [1, 2, 3]}
+    IO.inspect(contents)
+  end
+
+  def responseHandler(tuple) do
+    {status, contents} = tuple
+    case status do
+      :ok -> IO.inspect(contents)
+      _ -> IO.puts("Error")
+    end
+  end
+
+  # Does nothing, but shows off the pin operator
+  def isThirteen(x) do
+    ^x = 13
+  end
+
+end
